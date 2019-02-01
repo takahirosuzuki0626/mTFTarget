7f2645857b10c5c16e557630b8a055fa24d77612
takahirosuzuki1980 <takahirosuzuki1980@gmail.com>
2019-02-01 17:32:21 +0900

    add removal of non canonical sequences

--- R/DMREnhancer.R
+++ R/DMREnhancer.R
@@ -17,6 +17,7 @@ DMREnhancer <- function(sig_targets_gr){
 ## read GeneHancer data
 #gh_bed <- read.table("/osc-fs_home/t-suzuki/GeneHancer/GeneHancer_version_4_4_2_hg19.bed", header=F, stringsAsFactors=F)
 gh_bed <- gh_bed
+gh_bed <- gh_bed[grep("gl000",gh_bed[,1], invert=T),]    #removing GL000 seq
 gh_gr <- bed2granges(gh_bed)
 
 sig_enhancer_gr <- gh_gr[subjectHits(findOverlaps(sig_targets_gr, gh_gr))]    # intersect between enhancer and DMR
